d5ba6b57a79800ed01df5c8a982fc28271d53c71
Jeff Gregory <jeffrey.s.gregory@me.com>
2026-05-26 14:12:43 -0500

    Change __version__ setup in project

--- src/novastar_client/__init__.py
+++ src/novastar_client/__init__.py
@@ -2,7 +2,9 @@
 Importing all the modules
 """
 
-__version__ = "0.1.0"
+# __version__ = "1.0.0"
+# my_package/__init__.py
+from importlib.metadata import version, PackageNotFoundError
 
 from novastar_client import models
 from novastar_client import services
@@ -19,5 +21,10 @@ __all__ = [
     "NovaStarClient",
     "NovaStarConfig",
     "NovaStarSession",
-    "cli", # type: ignore
+    "cli",  # type: ignore
 ]
+
+try:
+    __version__ = version("my-package")
+except PackageNotFoundError:
+    __version__ = "v0.1.0"  # or some sensible default
